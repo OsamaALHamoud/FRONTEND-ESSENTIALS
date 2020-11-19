77bfa43da6b463601c2f57943618dc219ccfef39
Osama Al Hamoud <osama.alhamoud@student.rocva.nl>
2020-11-19 05:12:34 +0100

    file javascript toegevoegd

---
+++ opdracht/eindopdracht/javascript.js
@@ -0,0 +1,24 @@
+var age = prompt("Please enter your age",);
+
+if (age == 16) {
+document.querySelector("body").style.backgroundColor = "orange";
+
+}
+
+if (age == 17) {
+    document.querySelector("body").style.backgroundColor = "blue";
+}    
+
+
+
+if (age == 18) {
+    document.querySelector("body").style.backgroundColor = "gold";
+}    
+
+
+
+if (age == 19) {
+    document.querySelector("body").style.backgroundColor = "red";
+ 
+}
+
