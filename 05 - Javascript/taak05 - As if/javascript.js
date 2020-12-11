bea3f6acb72b615a75dd338cd6aa5d28ed6a990a
Your Name <you@example.com>
2020-12-11 13:28:34 +0100

    taak 5 van javascript voldaan

---
+++ 05 - Javascript/taak05 - As if/javascript.js
@@ -0,0 +1,11 @@
+let mijnLeeftijd = 18;
+ let stemGerechtigeleeftijd = 18;
+
+if(mijnLeeftijd==stemGerechtigeleeftijd){
+   
+   
+    prompt("vul u leeftijd in")
+    alert("Jij bent mag stemmen met de verkiezingen")
+   
+
+}
