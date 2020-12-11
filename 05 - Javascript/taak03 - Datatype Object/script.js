f966a126f77740ea91eaf9411dedb85de72ba84d
Your Name <you@example.com>
2020-12-11 12:21:22 +0100

    taak 4 van javascript string voldaan

---
+++ 05 - Javascript/taak03 - Datatype Object/script.js
@@ -0,0 +1,14 @@
+
+
+let persoon = {
+    voornaam: "osama",
+    achternaam: "al hamoud",
+    leeftijd: 24,
+    letikBenStemGerechtig: "ja",
+    ikMagLegaalAlcoholKopen: "ja mag niet legaalAlcohoelKopen",
+    ikMagBrommerRijden :"Ja  kan het rijden ",
+    SoftwareOntwikkeling : "een gooie beroep"
+
+}
+
+console.log (persoon);
---
+++ 05 - Javascript/taak04 - String kan plakken/javascript.js
@@ -0,0 +1,8 @@
+let mijNaamis = "osama";
+let mijnAchternaam =" ALhamoud ";
+let mijnLeeftijd = 24
+let volledigeZin = "mijn naam  is "+mijNaamis+" mijn achternaam"+mijnAchternaam+" mijn leeftijd " + mijnLeeftijd +" jaar oud";
+console.log(volledigeZin);
+
+
+
