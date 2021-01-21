b554eb6e3ad7585509ca1eede5f862788ecb6592
Your Name <you@example.com>
2021-01-21 11:02:42 +0100

    alle taken gemaakt

--- opdracht/eindopdracht/javascript.js
+++ opdracht/eindopdracht/javascript.js
@@ -1,37 +1,55 @@
 // var age = prompt("Please enter your age",);
 //vragen aan bezoeker
 //opslaan in object
+// let getaal = 50;
+
 let persoon ={
-    voornaam: prompt("Wat is je voornaam?")
-    //todo: andere properties
+    voornaam: prompt("Wat is je voornaam?"),
+    achterNaam: prompt ("wat is je achternaam"),
+    age: prompt ("wat is je leeftijd")
+    
 }
 
+
+let age = persoon.age;
+    
+    // todo: andere propertiesosamk
+
+
+
 //gegevens van object persoon in tabel zetten
-document.querySelector(".koptekst").innerHTML = persoon.voornaam;
+
+
+
+document.getElementById("naam").innerHTML = persoon.voornaam+" "+persoon.achterNaam,
+
+console.log(persoon.voornaam+" "+persoon.achterNaam+persoon.age);
+
+
 
 //gegevens van object persoon tonen in de console
 // check op persoon.leeftijd ipv age
-if (age == 16) {
+ if (age == 16) {
 document.querySelector("body").style.backgroundColor = "orange";
 
 }
-
-if (age == 17) {
+ 
+else if (age == 17) {
     document.querySelector("body").style.backgroundColor = "blue";
 }    
 
 
 
-if (age == 18) {
+else if (age == 18) {
     document.querySelector("body").style.backgroundColor = "gold";
 }    
 
 
 
-if (age == 19) {
+else if (age == 19) {
     document.querySelector("body").style.backgroundColor = "red";
  
 }
+else {document.querySelector("body").style.backgroundColor = "green"}
 
-//todo: if statement als er niks ingevuld
 
